6a073c0224eb5674716317efb30e05f31583dafd
Matt Wetmore <wetmore.matt@gmail.com>
2015-05-20 23:23:57 -0400

    Add switch function (kinda broken)

--- Keys.elm
+++ Keys.elm
@@ -1,10 +1,13 @@
 module Keys where
 
 import Keyboard exposing (KeyCode)
-import Signal exposing ((<~), (~))
+import Signal exposing ((<~), (~), map2)
 import Set
 import List
 import Graphics.Element exposing (..)
+
+import String
+import Char
 
 type Meta = Ctrl | Alt
 type Key  = Press Char | Esc | Ret | Chord (List Meta) Key
@@ -21,7 +24,19 @@ keys' = let
     fn (w,x) (_,z) = (w, x `Set.diff` z)
   in Signal.foldp fn emptyPair <| Set.partition isMeta <~ Keyboard.keysDown
 
+switch : Signal Bool -> Signal a -> Signal a -> Signal a
+switch b s s' = map2 (\t (a,a') -> if t then a else a') b <| (,) <~ s ~ s' -- triggers too many events
+
 keys = Signal.filter ((/=) emptyPair) emptyPair keys'
 
+metaDown (set, _) = List.length (Set.toList set) > 0
+
+s = switch (Signal.map metaDown keys) keys (Signal.map (\k -> (Set.empty, Set.singleton k)) Keyboard.presses)
+
 main : Signal Element
-main = (\x -> flow down (List.map show x)) <~ (Signal.foldp (::) [] keys)
+main = (\x -> flow down (List.map show x)) <~ (Signal.foldp (::) [] s)
+
+draw list = let
+    letters = List.map (\(_,x) -> List.map Char.fromCode <| Set.toList x) list
+    string = String.concat <| List.map String.fromList letters
+  in show (String.reverse string) 
